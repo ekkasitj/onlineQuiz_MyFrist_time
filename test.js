031a0acbe05f1bb0966b846bc5c507bf69bcb139
My Dream Come True <42135876+ekkasitj@users.noreply.github.com>
2020-12-12 19:21:51 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,26 @@
+/*function myFunction(x,y){
+    return x * y;
+}
+function addValue(a,b){
+    let c = a + b;
+    return c;
+}
+function alertBox(){
+    alert("Your Are so Handsom!");
+}*/
+//let prompT = prompt("Enter Your Username Please : ");
+
+let rabbit = {
+    name : 'Mimi',
+    bread : 'Red Head',
+    color : 'red slide side white',
+    weigth : 1.3
+}
+console.log(rabbit);
+
+let result = confirm('Please sign in ');
+if(result == true){
+    alert('We are welcome');
+}else{
+    alert('I love you very mush');
+}
